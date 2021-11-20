51ef170ff93f2f45025d2a4b6b24427bd5347cff
catalunha <catalunha.mj@gmail.com>
2021-11-20 08:59:32 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  var count = 0.obs;
+
+  Home({Key? key}) : super(key: key);
+  @override
+  Widget build(context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("counter"),
+        ),
+        body: Center(
+          child: Obx(
+            () => Text("$count"),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => count++,
+        ),
+      );
+}
